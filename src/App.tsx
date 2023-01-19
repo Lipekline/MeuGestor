6c821c7c2684cfdaadeebb86367dd6150ebc5e05
Luís Felipe Borges Silva <luis.silva@nanoincub.com.br>
2023-01-19 10:49:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>MEU GESTOR</Text>
+    </View>
+  );
+};
+
+export default App;
